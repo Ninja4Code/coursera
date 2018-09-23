91256a64ba537fc8d2c8c85625014a342bc2639b
Charles Owen <owen.charlesr@hotmail.com>
2018-09-23 19:33:30 -0400

    Fetch Handling Errors

--- src/redux/ActionCreators.js
+++ src/redux/ActionCreators.js
@@ -17,8 +17,24 @@ export const addComment = (dishId, rating, author, comment) => ({
 export const fetchDishes = () => (dispatch) => {
     dispatch(dishesLoading(true));
     return fetch(baseUrl + 'dishes')
-        .then(response => response.json())
-        .then(dishes => dispatch(addDishes(dishes)));
+    .then(response => {
+        // HTTP status code 200-299
+        if (response.ok) {
+          return response;
+        } else { // HTTP status code 400-599
+          let error = new Error('Error ' + response.status + ': ' + response.statusText);
+          error.response = response;
+          throw error;
+        }
+      }, // Didn't even communicate with server
+      error => {
+            let errmess = new Error(error.message);
+            //let errmess = new Error('Unable to retrieve dishes.  No connection to server.');
+            throw errmess;
+      })
+    .then(response => response.json())
+    .then(dishes => dispatch(addDishes(dishes)))
+    .catch(error => dispatch(dishesFailed(error.message)));
 }
 export const dishesLoading = () => ({
     type: ActionTypes.DISHES_LOADING
@@ -33,8 +49,23 @@ export const addDishes = (dishes) => ({
 });
 export const fetchComments = () => (dispatch) => {    
     return fetch(baseUrl + 'comments')
+    .then(response => {
+        // HTTP status code 200-299
+        if (response.ok) {
+          return response;
+        } else { // HTTP status code 400-599
+          let error = new Error('Error ' + response.status + ': ' + response.statusText);
+          error.response = response;
+          throw error;
+        }
+      }, // Didn't even communicate with server
+      error => {
+            let errmess = new Error(error.message);
+            throw errmess;
+      })
     .then(response => response.json())
-    .then(comments => dispatch(addComments(comments)));
+    .then(comments => dispatch(addComments(comments)))
+    .catch(error => dispatch(commentsFailed(error.message)));
 };
 export const commentsFailed = (errmess) => ({
     type: ActionTypes.COMMENTS_FAILED,
@@ -48,8 +79,23 @@ export const fetchPromos = () => (dispatch) => {
     dispatch(promosLoading());
 
     return fetch(baseUrl + 'promotions')
+    .then(response => {
+        // HTTP status code 200-299
+        if (response.ok) {
+          return response;
+        } else {  // HTTP status code 400-599
+          var error = new Error('Error ' + response.status + ': ' + response.statusText);
+          error.response = response;
+          throw error;
+        }
+      }, // Didn't even communicate with the server
+      error => {
+            var errmess = new Error(error.message);
+            throw errmess;
+      })
     .then(response => response.json())
-    .then(promos => dispatch(addPromos(promos)));
+    .then(promos => dispatch(addPromos(promos)))
+    .catch(error => dispatch(promosFailed(error.message)));
 }
 export const promosLoading = () => ({
     type: ActionTypes.PROMOS_LOADING
